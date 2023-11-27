50052979f695aa7f7f06b06e943cb92149c20e81
kkakk5996 <97136926+kkakk5996@users.noreply.github.com>
2023-11-27 22:42:30 +0900

    Add files via upload

---
+++ MainWindow.xaml.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h> // canvas
+#include <winrt/Microsoft.UI.Xaml.Input.h>
+#include <winrt/Microsoft.UI.Input.h> // mouse
+
+float px, py;
+
+namespace winrt::App2::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+    };
+}
+
+namespace winrt::App2::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
